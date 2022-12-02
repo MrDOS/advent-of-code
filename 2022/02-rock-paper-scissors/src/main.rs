0bf57e59402420145e841464141a0c0e2516c674
Samuel Coleman <samuel@seenet.ca>
2022-12-02 06:19:44 +0000

    2022D2P2.

--- 2022/02-rock-paper-scissors/src/main.rs
+++ 2022/02-rock-paper-scissors/src/main.rs
@@ -1,25 +1,61 @@
 use std::{io::stdin, str::FromStr};
 
+#[derive(Clone, Copy)]
 enum Outcome {
     Loss = 0,
     Draw = 3,
     Win = 6,
 }
 
-#[derive(Eq, Hash, PartialEq)]
+impl Outcome {
+    fn play(self, other: TheirPlay) -> i32 {
+        let mut score = self as i32;
+
+        score += match self {
+            Self::Loss => match other {
+                TheirPlay::Rock => MyPlay::Scissors,
+                TheirPlay::Paper => MyPlay::Rock,
+                TheirPlay::Scissors => MyPlay::Paper,
+            },
+            Self::Draw => match other {
+                TheirPlay::Rock => MyPlay::Rock,
+                TheirPlay::Paper => MyPlay::Paper,
+                TheirPlay::Scissors => MyPlay::Scissors,
+            },
+            Self::Win => match other {
+                TheirPlay::Rock => MyPlay::Paper,
+                TheirPlay::Paper => MyPlay::Scissors,
+                TheirPlay::Scissors => MyPlay::Rock,
+            },
+        } as i32;
+
+        return score;
+    }
+}
+
+impl FromStr for Outcome {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Outcome, Self::Err> {
+        match input {
+            "X" => Ok(Self::Loss),
+            "Y" => Ok(Self::Draw),
+            "Z" => Ok(Self::Win),
+            _ => Err(()),
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
 enum MyPlay {
-    Rock,
-    Paper,
-    Scissors,
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3,
 }
 
 impl MyPlay {
     fn play(self, other: TheirPlay) -> i32 {
-        let mut score = match self {
-            Self::Rock => 1,
-            Self::Paper => 2,
-            Self::Scissors => 3,
-        };
+        let mut score = self as i32;
 
         score += match self {
             Self::Rock => match other {
@@ -56,6 +92,7 @@ impl FromStr for MyPlay {
     }
 }
 
+#[derive(Clone, Copy)]
 enum TheirPlay {
     Rock,
     Paper,
@@ -76,14 +113,20 @@ impl FromStr for TheirPlay {
 }
 
 fn main() {
-    let mut score = 0;
+    let mut play_score = 0;
+    let mut outcome_score = 0;
 
     for line in stdin().lines().map(|l| l.unwrap()) {
         let mut plays = line.split_whitespace();
         let theirs = TheirPlay::from_str(plays.next().unwrap()).unwrap();
-        let mine = MyPlay::from_str(plays.next().unwrap()).unwrap();
-        score += mine.play(theirs);
+
+        let second = plays.next().unwrap();
+        let mine = MyPlay::from_str(second).unwrap();
+        let outcome = Outcome::from_str(second).unwrap();
+        play_score += mine.play(theirs);
+        outcome_score += outcome.play(theirs);
     }
 
-    println!("{}", score);
+    println!("Total score if the second column gives plays: {}", play_score);
+    println!("Total score if the second column gives outcomes: {}", outcome_score);
 }
