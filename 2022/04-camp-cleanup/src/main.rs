5c1086d306292bfb9b13fe3ca33dfb9e6473bc15
Samuel Coleman <samuel@seenet.ca>
2022-12-04 22:17:41 +0000

    2022D4P2.

--- 2022/04-camp-cleanup/src/main.rs
+++ 2022/04-camp-cleanup/src/main.rs
@@ -21,10 +21,16 @@ impl ContainableRange {
     fn contains(&self, other: &ContainableRange) -> bool {
         return self.start <= other.start && self.end >= other.end;
     }
+
+    fn overlaps(&self, other: &ContainableRange) -> bool {
+        return (self.start >= other.start && self.start <= other.end)
+            || (self.end >= other.start && self.end <= other.end);
+    }
 }
 
 fn main() {
     let mut contained = 0;
+    let mut overlapping = 0;
 
     for line in stdin().lines().map(|line| line.unwrap()) {
         let pieces: Vec<_> = line
@@ -32,10 +38,16 @@ fn main() {
             .take(2)
             .map(|range| Into::<ContainableRange>::into(range))
             .collect();
+
         if pieces[0].contains(&pieces[1]) || pieces[1].contains(&pieces[0]) {
             contained += 1;
         }
+
+        if pieces[0].overlaps(&pieces[1]) || pieces[1].overlaps(&pieces[0]) {
+            overlapping += 1;
+        }
     }
 
-    println!("{}", contained);
+    println!("Fully-contained assignment pairs: {}", contained);
+    println!("Partially-overlapping assignment pairs: {}", overlapping);
 }
